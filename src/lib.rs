60aa3a3d3c2adc8f23c4e36f261220c46988fa09
Striker2783 <owenxingli@gmail.com>
2023-06-08 19:51:29 -0400

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,53 @@
+use std::{
+    env::{self, Args},
+    error::Error,
+};
+
 pub mod hash_mult_impl;
+
+pub struct Config {
+    tax: String,
+    fasta: String,
+    k_fold: u32,
+}
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            tax: Default::default(),
+            fasta: Default::default(),
+            k_fold: 5,
+        }
+    }
+}
+impl Config {
+    pub fn build(mut args: Args) -> Result<Self, Box<dyn Error>> {
+        args.next();
+        let tax = args.next().ok_or("No tax argument")?;
+        let fasta = args.next().ok_or("No Fasta argument")?;
+        if let Some(k) = args.next() {
+            return Ok(Config {
+                tax,
+                fasta,
+                k_fold: k.parse()?,
+            });
+        }
+
+        Ok(Config {
+            tax,
+            fasta,
+            ..Default::default()
+        })
+    }
+
+    pub fn tax(&self) -> &str {
+        self.tax.as_ref()
+    }
+
+    pub fn fasta(&self) -> &str {
+        self.fasta.as_ref()
+    }
+
+    pub fn k_fold(&self) -> u32 {
+        self.k_fold
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
-use k_fold_cross_validation::hash_mult_impl::{
-    data_set::Total as MultTotal, Storage as MultStorage,
+use k_fold_cross_validation::{
+    hash_mult_impl::{data_set::Total as MultTotal, Storage as MultStorage},
+    Config,
 };
-use std::{fs, path::Path, time::Instant};
+use std::{env, fs, path::Path, process, time::Instant};
 
-fn hash_mult_impl() {
+fn hash_mult_impl(config: &Config) {
     let time = Instant::now();
     let mut storage = MultStorage::default();
 
-    if let Err(e) = storage.load_tax_file("TAX.txt") {
+    if let Err(e) = storage.load_tax_file(config.tax()) {
         eprintln!("{e}");
     }
-    if let Err(e) = storage.load_fasta_file("FASTA.txt") {
+    if let Err(e) = storage.load_fasta_file(config.fasta()) {
         eprintln!("{e}");
     };
-    let data_set = MultTotal::build(&storage, 5);
+    let data_set = MultTotal::build(&storage, config.k_fold());
 
     let path = Path::new("files");
     if let Err(e) = fs::create_dir(path) {
@@ -25,7 +26,14 @@ fn hash_mult_impl() {
 
     println!("Hash Concurrency: {} ms", time.elapsed().as_millis());
 }
-
 fn main() {
-    hash_mult_impl();
+    let args = env::args();
+    let config = match Config::build(args) {
+        Ok(config) => config,
+        Err(e) => {
+            eprintln!("{e}");
+            process::exit(1);
+        }
+    };
+    hash_mult_impl(&config);
 }
